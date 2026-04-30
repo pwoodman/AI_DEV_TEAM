beabc2593e391b599baab39a005efc5395dca48b
Patrick Woodman <patrick.woodman.1990@gmail.com>
2026-04-29 22:33:13 -0400

    feat(validators): register Rust, Java, C#, Next.js adapters in registry

--- packages/validators/src/index.ts
+++ packages/validators/src/index.ts
@@ -15,8 +15,12 @@ export * from "./lang-adapter-validator.js";
 export * from "./spawn-command.js";
 export * from "./forward-risk.js";
 
+import { csharpAdapter } from "./adapters/csharp.js";
 import { goAdapter } from "./adapters/go.js";
+import { javaAdapter } from "./adapters/java.js";
+import { nextjsAdapter } from "./adapters/nextjs.js";
 import { pythonAdapter } from "./adapters/python.js";
+import { rustAdapter } from "./adapters/rust.js";
 import { typescriptAdapter } from "./adapters/typescript.js";
 import { adapterRegistry } from "./lang-adapter-registry.js";
 import { makeLangAdapterValidator } from "./lang-adapter-validator.js";
@@ -24,9 +28,17 @@ import { makeLangAdapterValidator } from "./lang-adapter-validator.js";
 adapterRegistry.register(typescriptAdapter);
 adapterRegistry.register(pythonAdapter);
 adapterRegistry.register(goAdapter);
+adapterRegistry.register(rustAdapter);
+adapterRegistry.register(javaAdapter);
+adapterRegistry.register(csharpAdapter);
+adapterRegistry.register(nextjsAdapter);
 
+export { csharpAdapter } from "./adapters/csharp.js";
 export { goAdapter } from "./adapters/go.js";
+export { javaAdapter } from "./adapters/java.js";
+export { nextjsAdapter } from "./adapters/nextjs.js";
 export { pythonAdapter } from "./adapters/python.js";
+export { rustAdapter } from "./adapters/rust.js";
 export { typescriptAdapter } from "./adapters/typescript.js";
 
 export const langAdapterValidator = makeLangAdapterValidator(adapterRegistry);
